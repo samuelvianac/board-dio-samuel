fe04d8c2c43f459713257e7de290d2f82ae0b1da
samuelvianac <samuelvianac3@gmail.com>
2026-04-19 18:48:33 -0300

    adiciona executor de migrations com liquibase

---
+++ src/main/java/br/com/dio/board/config/DatabaseMigrationManager.java
@@ -0,0 +1,33 @@
+package br.com.dio.board.config;
+
+import liquibase.Contexts;
+import liquibase.LabelExpression;
+import liquibase.Liquibase;
+import liquibase.database.Database;
+import liquibase.database.DatabaseFactory;
+import liquibase.database.jvm.JdbcConnection;
+import liquibase.resource.ClassLoaderResourceAccessor;
+
+import java.sql.Connection;
+
+public class DatabaseMigrationManager {
+
+    private DatabaseMigrationManager() {
+    }
+
+    public static void migrate() throws Exception {
+        try (Connection connection = ConnectionFactory.getConnection()) {
+            Database database = DatabaseFactory.getInstance()
+                    .findCorrectDatabaseImplementation(new JdbcConnection(connection));
+
+            Liquibase liquibase = new Liquibase(
+                    "db/changelog/db.changelog-master.xml",
+                    new ClassLoaderResourceAccessor(),
+                    database
+            );
+
+            liquibase.update(new Contexts(), new LabelExpression());
+            System.out.println("Migrações aplicadas com sucesso.");
+        }
+    }
+}
